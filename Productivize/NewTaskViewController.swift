4b35807a653518a35244548cf6661efb81df2d8d
Bhairavi Chandersekhar <bhair@bhairavis-macbook-pro.local>
2017-05-14 20:21:00 -0400

    added a new task view controller which can accessed through a popover segue and be dismissed using either the cancel button or the add button

---
+++ Productivize/NewTaskViewController.swift
@@ -0,0 +1,45 @@
+//
+//  NewTaskViewController.swift
+//  Productivize
+//
+//  Created by Bhairavi Chandersekhar on 5/14/17.
+//  Copyright © 2017 Bhairavi Chandersekhar. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+
+    @IBAction func dismissCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        print("Dismiss Cancel")
+    }
+    
+    @IBAction func dismissAdd(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        print("Dismiss Add")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

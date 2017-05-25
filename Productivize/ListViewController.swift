bfbafdbbf7e380ff593a106ac4c37b4b5ff4e4c3
Bhairavi Chandersekhar <bhair@bhairavis-macbook-pro.local>
2017-05-24 20:26:08 -0400

    there is now a timer in the list view it has NOTHING to do with anything but you can time a minute the start, pause/resume, and reset buttons enable and disable themselves appropriately (a little bit too much, maybe, but that's okay)

--- Productivize/ListViewController.swift
+++ Productivize/ListViewController.swift
@@ -10,9 +10,75 @@ import UIKit
 
 class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
     var taskList: [Task]?
+    
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    var resumeTapped = false
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        if isTimerRunning == false {
+            runTimer()
+            startButton.isEnabled = false
+            resetButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+        if self.resumeTapped == false {
+            timer.invalidate()
+            resumeTapped = true
+            pauseButton.setTitle("Resume",for: .normal)
+        } else {
+            runTimer()
+            resumeTapped = false
+            pauseButton.setTitle("Pause",for: .normal)
+        }
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        seconds = 60
+        timerLabel.text = timeString(time: TimeInterval(seconds))
+        
+        isTimerRunning = false
+        pauseButton.isEnabled = false
+        startButton.isEnabled = true
+        resetButton.isEnabled = false
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ListViewController.updateTimer)), userInfo: nil, repeats: true)
+        
+        isTimerRunning = true
+        pauseButton.isEnabled = true
+    }
+    
+    func updateTimer() {
+        if seconds < 1 {
+            timer.invalidate()
+            //Send alert
+        } else {
+            seconds -= 1
+            timerLabel.text = timeString(time: TimeInterval(seconds))
+        }
+    }
+    
+    func timeString(time: TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        
+        return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +92,8 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
         if taskList!.count > 0 {
             return
         }
+        
+        pauseButton.isEnabled = false
     }
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
